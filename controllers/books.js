87579f781b1cf43b6a6bb1cbe3c51bca9f64d75e
Krystiane Otis <oti19001@byui.edu>
2023-02-11 20:19:06 -0700

    delete and put added and error handling

--- controllers/books.js
+++ controllers/books.js
@@ -2,6 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+  try{
   const result = await mongodb
     .getDb()
     .db('booksDB')
@@ -11,56 +12,74 @@ const getAll = async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
+  } catch (error){
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching books' });
+  }
 };
 
+
 const getSingle = async (req, res) => {
-  const bookId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db('booksDB')
-    .collection('books')
-    .find({ _id: bookId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const bookId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db('booksDB')
+      .collection('books')
+      .find({ _id: bookId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching a book' });
+  }
 };
 
 
 const getAddBook = async (req, res) => {
-const book = {
-    title: req.body.title,
-    author: req.body.author,
-    genre: req.body.genre,
-    isbn: req.body.isbn
-  };
-  const response = await mongodb.getDb().db('booksDB').collection('books').insertOne(book);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the book.');
+  try {
+    const book = {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      isbn: req.body.isbn
+    };
+    const response = await mongodb.getDb().db('booksDB').collection('books').insertOne(book);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json({ error: 'An error occurred while creating the book' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while creating the book' });
   }
 };
 
 const updateBook = async (req, res) => {
-  const bookId = new ObjectId(req.params.id);
-  // be aware of updateOne if you only want to update specific fields
-  const book = {
-    title: req.body.title,
-    author: req.body.author,
-    genre: req.body.genre,
-    isbn: req.body.isbn
-  };
-  const response = await mongodb
-    .getDb()
-    .db('booksDB')
-    .collection('books')
-    .replaceOne({ _id: bookId }, book);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the book.');
+  try {
+    const bookId = new ObjectId(req.params.id);
+    const book = {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      isbn: req.body.isbn
+    };
+    const response = await mongodb
+      .getDb()
+      .db('booksDB')
+      .collection('books')
+      .replaceOne({ _id: bookId }, book);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      throw new Error('Error occurred while updating the book.');
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
